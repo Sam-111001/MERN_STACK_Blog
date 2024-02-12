1b0a140132522825889f44d35c6d28312e3adce7
Shamant <code.shamant@gmail.com>
2024-02-12 12:26:43 +0530

    update header component with user data

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -15,7 +15,7 @@ const userSlice = createSlice({
       state.error = null;
     },
     signInSuccess: (state, action) => {
-      state.cuttentUser = action.payload;
+      state.currentUser = action.payload;
       state.loading = false;
       state.error = null;
     },
